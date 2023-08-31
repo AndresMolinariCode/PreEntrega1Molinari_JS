ef0b1bbb914388c9323405f62117f71075a516c8
Andrés Molinari <amolinarisalvo@gmail.com>
2023-08-31 16:19:40 -0300

    Creado menu interactivo de boton Finalizar Compra

--- js/script.js
+++ js/script.js
@@ -1,46 +1,8 @@
-//OPCION 1
-// const products = [
-//   {
-//       id: 1,
-//       name: "Producto 1",
-//       image: "./img/Suplementos/WheyProtein/WheyProtein_1-removebg-preview.png",
-//       price: 10.99
-//   },
-//   {
-//       id: 2,
-//       name: "Producto 2",
-//       image: "./img/Suplementos/WheyProtein/WheyProtein_2-removebg-preview.png",
-//       price: 15.99
-//   },
-//   // Agrega más productos aquí
-// ];
-
-// const productGrid = document.getElementById("product-grid");
-
-// products.forEach(product => {
-//   const productCard = document.createElement("div");
-//   productCard.classList.add("col-md-4", "mb-4");
-
-//   productCard.innerHTML = `
-//       <div class="card">
-//           <img src="${product.image}" class="card-img-top" alt="${product.name}">
-//           <div class="card-body">
-//               <h5 class="card-title">${product.name}</h5>
-//               <p class="card-text">$${product.price.toFixed(2)}</p>
-//               <button class="btn btn-primary" onclick="addToCart(${product.id})">Agregar al carrito</button>
-//           </div>
-//       </div>
-//   `;
-
-//   productGrid.appendChild(productCard);
-// });
-
-// function addToCart(productId) {
-//   // Aquí puedes implementar la lógica para agregar el producto al carrito
-//   console.log(`Producto con ID ${productId} agregado al carrito.`);
-// }
-
+//CODIGO PARA MOSTRAR GRILLA DE PRODUCTOS Y AGREGAR PRODUCTOS AL CARRO
+//INICIO
+//Uso de constantes
 const products = [
+  // Aquí se agregan los productos a ser listados  
   {
       image: '../img/Suplementos/WheyProtein/WheyProtein_1-removebg-preview.png',
       title: 'Producto 1',
@@ -59,29 +21,15 @@ const products = [
       description: 'Descripción del Producto 3',
       price: 29.99
   },
-  // Agrega más productos aquí
+  
 ];
 
 const productGrid = document.getElementById('productGrid');
 
+//Uso de bucles
 products.forEach((product, index) => {
   const productCard = document.createElement('div');
   productCard.className = 'col-md-4 mb-4';
-
-//   productCard.innerHTML = `
-//       <div class="card">
-//           <img src="${product.image}" class="card-img-top" alt="${product.title}">
-//           <div class="card-body">
-//               <h5 class="card-title">${product.title}</h5>
-//               <p class="card-text">${product.description}</p>
-//               <p class="card-text">$${product.price.toFixed(2)}</p>
-//               <div class="input-group mb-3">
-//                   <input type="number" class="form-control" value="1" min="1" id="quantity${index}">
-//                   <button class="btn btn-primary" onclick="addToCart(${index})">Agregar al carrito</button>
-//               </div>
-//           </div>
-//       </div>
-//   `;
 
     productCard.innerHTML = `
         <div class="card">
@@ -98,12 +46,12 @@ products.forEach((product, index) => {
         </div>
     `;
 
-
   productGrid.appendChild(productCard);
 });
 
 const cart = [];
 
+//Uso de funciones
 function addToCart(index) {
   const quantity = parseInt(document.getElementById(`quantity${index}`).value);
   if (quantity > 0) {
@@ -115,3 +63,136 @@ function addToCart(index) {
       document.getElementById(`quantity${index}`).value = 1;
   }
 }
+
+//FIN
+
+//CODIGO PARA CUMPLIMIENTO DE REQUISITOS DE PRE ENTREGA
+//INICIO
+
+//-->CREACION DE FUNCIONES<--
+function finalizarCompra() {
+
+    ////-->USO DE FUNCIONES DECLARADAS POSTERIORMENTE <--
+    const nombreUsuario = pedirNombre();
+
+    //-->USO DE CONDICIONALES<--
+    if (nombreUsuario !== null) {
+        alert(`Hola, ${nombreUsuario}! Vamos a finalizar la compra.`);
+    }
+
+    const medioPagoSeleccionado = solicitarMedioDePago();
+    
+    if (medioPagoSeleccionado !== null) {
+        let mensaje = `Has seleccionado: ${medioPagoSeleccionado}`;
+
+        // Calcular el total de la compra si el pago es efectivo/débito
+        if (medioPagoSeleccionado === "Efectivo/Débito") {
+            const totalCompra = calcularTotalCompra();
+            mensaje += `\nTotal de la compra: $${totalCompra.toFixed(2)}`;
+        } else if (medioPagoSeleccionado.startsWith("Tarjeta de Crédito")) {
+            const cuotas = obtenerCantidadCuotas(medioPagoSeleccionado);
+            const totalCompra = calcularTotalCompra();
+            const precioPorCuota = totalCompra / cuotas;
+            mensaje += `\nTotal de la compra: $${totalCompra.toFixed(2)}`;
+            mensaje += `\nCantidad de cuotas: ${cuotas}`;
+            mensaje += `\nPrecio por cuota: $${precioPorCuota.toFixed(2)}`;
+        }
+
+        alert(mensaje);       
+    }    
+}
+
+function pedirNombre() {
+    let nombre = "";
+    let valido = false;
+
+    while (!valido) {
+        nombre = prompt("Por favor, ingresa tu nombre:");
+
+        if (nombre === null) {
+            // El usuario canceló la entrada
+            return null;
+        }
+
+        // Validar que el nombre no esté vacío y solo contenga letras
+        if (nombre.trim() === "") {
+            alert("El nombre no puede estar vacío. Por favor, ingresa tu nombre.");
+        } else if (!/^[A-Za-zÁ-ÿ\s]+$/.test(nombre)) {
+            alert("El nombre solo puede contener letras. Por favor, ingresa un nombre válido.");
+        } else {
+            valido = true;
+        }
+    }
+
+    return nombre;
+}
+
+function solicitarMedioDePago() {
+    let medioPago = "";
+    let valido = false;
+
+    while (!valido) {
+        medioPago = prompt("Selecciona el medio de pago:\n1 - Efectivo/Débito\n2 - Tarjeta de Crédito");
+
+        if (medioPago === null) {
+            // El usuario canceló la entrada
+            return null;
+        }
+
+        if (medioPago !== "1" && medioPago !== "2") {
+            alert("Opción no válida. Por favor, selecciona 1 para Efectivo/Débito o 2 para Tarjeta de Crédito.");
+        } else {
+            valido = true;
+        }
+    }
+
+    if (medioPago === "2") {
+        let cuotas = "";
+        valido = false;
+
+        while (!valido) {
+            cuotas = prompt("Selecciona la cantidad de cuotas:\n2, 4, 6 u 12");
+
+            if (cuotas === null) {
+                // El usuario canceló la entrada
+                return null;
+            }
+
+            if (cuotas !== "2" && cuotas !== "4" && cuotas !== "6" && cuotas !== "12") {
+                alert("Opción no válida. Por favor, selecciona 2, 4, 6 u 12 para la cantidad de cuotas.");
+            } else {
+                valido = true;
+            }
+        }
+
+        return `Tarjeta de Crédito (${cuotas} cuotas)`;
+    } else {
+        return "Efectivo/Débito";
+    }
+}
+
+// Función para calcular el total de la compra
+function calcularTotalCompra() {
+    let total = 0;
+    for (const item of cart) {
+        total += item.product.price * item.quantity;
+    }
+    return total;
+}
+
+// Función para obtener la cantidad de cuotas
+function obtenerCantidadCuotas(medioPagoSeleccionado) {
+    let partes = medioPagoSeleccionado.split(" ")
+    return parseInt(partes[3].replace("(", ""));    
+}
+
+// Esperar a que el documento esté completamente cargado
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener referencia al botón
+    const finalizarCompraBtn = document.getElementById("finalizarCompraBtn");
+
+    // Agregar evento de clic al botón
+    finalizarCompraBtn.addEventListener("click", finalizarCompra);
+});
+
+//FIN
